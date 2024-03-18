82e90748b794a0af6ebdd6e5534eecd5f8bc73a0
3mR <amrgouda127@gmail.com>
2024-03-18 05:53:49 +0200

    update

--- src/components/AitoolsPage/ContentAi.jsx
+++ src/components/AitoolsPage/ContentAi.jsx
@@ -2,8 +2,7 @@ import React, { useState } from 'react';
 import api from './api.json';
 import { GoLinkExternal } from "react-icons/go";
 import LogoIcon from '../../assets/png/logo.png'
-import { IoIosMenu } from "react-icons/io";
-import { IoCloseSharp } from "react-icons/io5";
+import { IoMdClose, IoMdMenu } from "react-icons/io";
 import { Link } from 'react-router-dom';
 const ContentAi = () => {
     const NavLinks = [
@@ -20,50 +19,43 @@ const ContentAi = () => {
             link: "/ai-tools"
         },
     ]
-    const [close, setclose] = useState(true);
-    const toggleclose = () => {
-        setclose(!close)
+
+    const [openMenue, setOpenMenue] = useState(false);
+    const toggleMenue = () => {
+        setOpenMenue(!openMenue)
     }
 
     return (
-        <div className=''>
-            <div className="fixed bg-white w-full py-2 top-0 max-sm:px-5 px-28 shadow-md flex justify-between ">
-                {/* logo section */}
-                <div className=''>
-                    <Link to='/' className='  font-logos font-bold flex lg:w-full lg:text-3xl max-sm:text-2xl md:w-1/6 md:text-xl text-nowrap '>&lt; Begin <img src={LogoIcon} className='max-sm:w-8 md:w-5 lg:w-10' alt="" />Front &gt;</Link>
+        <div className='xl:w-11/12 w-full xl:mx-auto ' >
+       
+            <div className="navbar px-7 shadow-md fixed top-0 right-0 left-0      xl:mx-auto py-5 xl:py-2  xl:bg-white bg-[#F4F4F5]">
+            <div className='  flex xl:w-11/12 flex-col w-full xl:flex-row xl:items-center xl:justify-between mx-auto gap-2'>
+                <div className="logo flex items-center justify-between text-5xl"><div className="flex items-center">Begin <img src={LogoIcon} className='inline-block w-[50px]' alt="" /><span className='font-bold'>Front</span></div> <div className='max-sm:block hidden w-20 '></div>
+                    <div className='' onClick={() => toggleMenue()}> {!openMenue ? <IoMdMenu className='block xl:hidden' /> : <IoMdClose className='block xl:hidden' />}
+                    </div>
                 </div>
-                <div className="iconNav max-sm:block text-4xl hidden" onClick={() => toggleclose()}>
-                    {
-                        close ? <IoIosMenu /> : <IoCloseSharp />
-                    }
+
+                <div className={`navLinks absolute shadow-lg font-bold font-hero xl:shadow-none xl:static xl:w-auto w-full xl:bg-white  p-5 bg-[#F4f3f5] ${openMenue ? 'block' : 'hidden'} xl:block top-[86px] left-0`}>
+                    <ul className={`flex flex-col  xl:flex-row  gap-4 xl:text-center  xl:gap-10 justify-center text-2xl `} >{NavLinks.map((link, i) => (<li className='cursor-pointer xl:hover:scale-125 duration-200'  key={i}>{link.name}</li>))}</ul>
                 </div>
-                {/* links */}
-
-
-                <ul className={`flex max-sm:absolute max-sm:flex-col max-sm:py-5 max-sm:shadow-lg max-sm:bg-slate-200 max-sm:w-full max-sm:-translate-x-5  max-sm:translate-y-11  lg:text-xl font-bold uppercase max-sm:justify-around max-sm:gap-4 lg:gap-20 items-center duration-200 ${close ? '-left-96' : 'left-5'}`} >
-                    {
-                        NavLinks.map((item) => (
-                            <li><a className='cursor-pointer' href={item.link}>{item.name}</a></li>
-                        ))
-                    }
-                </ul>
-
-
             </div>
 
 
 
-            <div className='container flex items-center mt-16 justify-center min-h-screen mx-auto'>
+        </div>
+
+
+            <div className=' flex items-center mt-28 justify-center  mx-auto'>
                 {/* grid system */}
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-4 ">
                     {
                         api.map((item, i) => (
                             <div className="card my-5 mx-2  bg-white border-solid border-gray-100 border-2   rounded-xl p-5 shadow-lg  ">
                                 <div className="flex flex-col gap-4">
-                                    <a href={item.toolSite}><img src={item.imgSrc} className='cursor-pointer rounded-xl overflow-hidden' alt="" /></a>
+                                    <a href={item.toolSite}><img src={item.imgSrc} className='cursor-pointer rounded-xl overflow-hidden w-full' alt="" /></a>
                                   <button className='bg-[#4B6BFB] bg-opacity-5 px-4 text-[#4B6BFB] rounded-md w-auto mx-auto capitalize  text-nowrap flex items-center'><a className='' href={item.toolSite}>{item.toolName}</a> <GoLinkExternal/> </button>  
-                                    <p className='text-slate-600 lg:leading-tight'>{item.toolDescSm}</p>
-                                    <button  className='text-white rounded-md    px-4 bg-[#4B6BFB]  capitalize text-xl text-nowrap'>More Details</button>
+                                    <p className='text-slate-600 lg:leading-tight text-xl'>{item.toolDescSm}</p>
+                                    <Link to={'details'}  className='text-white rounded-md py-2 text-center   px-4 bg-[#4B6BFB] hover:bg-opacity-5 hover:text-[#4b6bfb] duration-200  capitalize text-xl text-nowrap'>More Details</Link>
 
                                 </div>
                             </div>
