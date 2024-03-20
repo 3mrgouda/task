031815acbb7aaa618507fe8e30111f7ad3b3edf3
3mR <amrgouda127@gmail.com>
2024-03-20 17:28:30 +0200

    edit navbar

--- src/components/AitoolsPage/ContentAi.jsx
+++ src/components/AitoolsPage/ContentAi.jsx
@@ -29,18 +29,16 @@ const ContentAi = () => {
     const handleDynamic=(id)=>{
         setDynamic(id)
     }
-{
-    console.log(dynamic)
-}
+
     return (
         <div className=' w-full xl:mx-auto ' >
        
             <div className="navbar px-7 shadow-md fixed top-0 right-0 left-0      xl:mx-auto py-5 xl:py-2  xl:bg-white bg-[#F4F4F5]">
             <div className='  flex xl:w-11/12 flex-col w-full xl:flex-row xl:items-center xl:justify-between mx-auto gap-2'>
-                <Link to={'../../'} className="logo flex items-center justify-between text-4xl sm:text-5xl"><div className="flex items-center">Begin <img src={LogoIcon} className='inline-block w-[50px]' alt="" /><span className='font-bold'>Front</span></div> <div className='max-sm:block hidden w-20 '></div>
+                <div  className="logo flex items-center justify-between text-4xl sm:text-5xl"><Link to={'../../'} className="flex items-center">Begin <img src={LogoIcon} className='inline-block w-[50px]' alt="" /><span className='font-bold'>Front</span></Link> <div className='max-sm:block hidden w-20 '></div>
                     <div className='' onClick={() => toggleMenue()}> {!openMenue ? <IoMdMenu className='block xl:hidden' /> : <IoMdClose className='block xl:hidden' />}
                     </div>
-                </Link>
+                </div>
 
                 <div className={`navLinks absolute shadow-lg font-bold font-hero xl:shadow-none xl:static xl:w-auto w-full xl:bg-white  p-5 bg-[#F4f3f5] ${openMenue ? 'block' : 'hidden'} xl:block top-[86px] left-0`}>
                     <ul className={`flex flex-col  xl:flex-row  gap-4 xl:text-center  xl:gap-10 justify-center text-2xl `} >{NavLinks.map((link, i) => (<li className='cursor-pointer xl:hover:scale-125 duration-200'  key={i}>{link.name}</li>))}</ul>
@@ -60,7 +58,7 @@ const ContentAi = () => {
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-4 ">
                     {
                         api.map((item, i) => (
-                            <div className="card my-5 mx-2  bg-white border-solid border-gray-100 border-2   rounded-xl p-5 shadow-lg  ">
+                            <div key={i} className="card my-5 mx-2  bg-white border-solid border-gray-100 border-2   rounded-xl p-5 shadow-lg  ">
                                 <div className="flex flex-col gap-4">
                                     <a href={item.toolSite}><img src={item.imgSrc} className='cursor-pointer rounded-xl overflow-hidden w-full' alt="" /></a>
                                   <button className='bg-[#4B6BFB] bg-opacity-5 px-4 text-[#4B6BFB] rounded-md w-auto mx-auto capitalize  text-nowrap flex items-center'><a className='' href={item.toolSite}>{item.toolName}</a> <GoLinkExternal/> </button>  
--- src/components/navar/Navbar.jsx
+++ src/components/navar/Navbar.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FaCaretDown, FaUser } from 'react-icons/fa';
 import LogoIcon from '../../assets/png/logo.png';
 import { Link } from 'react-router-dom';
+import { IoMdClose, IoMdMenu } from 'react-icons/io';
 const navLink = [
   {
     id: 1,
@@ -37,45 +38,43 @@ const dropDownLinks = [
   },
 ]
 const Navbar = ({ HandlePopUP }) => {
+  const [openMenue, setOpenMenue] = useState(false);
+  const toggleMenue = () => {
+    setOpenMenue(!openMenue)
+  }
+
   return (<>
-    <div data-aos="fade" className='shadow-md bg-white  '>
-      <div className="cotainer flex justify-around md:justify-between items-center py-4 sm:py-3 px-20">
-        {/* logo section */}
-        <div>
-          <a href="#" className='font-logos font-bold flex lg:w-full lg:text-3xl max-sm:text-xl md:w-1/6 md:text-xl text-nowrap '>&lt; Begin <img src={LogoIcon} className='max-sm:w-8 md:w-5 lg:w-10' alt="" />Front &gt;</a>
+ 
+    <div  className='fixed bg-white -mt-1 py-1  flex xl:w-11/12 flex-col w-full xl:flex-row xl:items-center xl:justify-between mx-auto gap-2'>
+      <Link to={'../../'} className="logo w-10/12 mx-auto flex items-center justify-between text-4xl sm:text-5xl"><div className="flex items-center">Begin <img src={LogoIcon} className='inline-block w-[50px]' alt="" /><span className='font-bold'>Front</span></div> <div className='max-sm:block hidden w-20 '></div>
+        <div className='' onClick={() => toggleMenue()}> {!openMenue ? <IoMdMenu className='block xl:hidden' /> : <IoMdClose className='block xl:hidden' />}
         </div>
-        {/* links section */}
-        <div>
-          <ul className='flex items-center gap-10 '>
-            {
-              navLink.map(({ id, name, link }) => (
-                <li key={id}>
-                  <Link to={link} className='hidden md:inline-block hover:text-primary font-semibold text-xl hover:bg-black/15'>{name}</Link>
-                </li>
-              ))}
-            {/* simple drop-down and links  */}
-            <li className='cursor-pointer group hidden md:block'>
-              <a href="" className='inline-block hover:text-primary font-semibold text-xl'>
-                <div className='flex items-center gap-[2px] py-2'>Category <span><FaCaretDown className='group-hover:rotate-180 duration-300' /></span></div>
-              </a>
-              {/* drop-down section */}
-              <div className='absolute z-50 hidden group-hover:block w-[200px] shadow-md p-2'>
-                <ul>{
+      </Link>
 
-                  dropDownLinks.map(({ id, name, link }) => (
-                    <li key={id}>
-                      <Link to={link} className='inline-block w-full rounded-md p-2  hover:bg-primary/20 font-semibold text-xl'>{name}</Link>
+      <div className={`navLinks -z-10 absolute shadow-lg font-bold font-hero xl:shadow-none xl:static xl:w-auto w-full xl:bg-white  p-5 bg-[#F4f3f5] ${openMenue ? 'block' : 'hidden'} xl:block top-[55px] left-0`}>
+        <ul className={`flex flex-col  xl:flex-row  gap-4 xl:text-center  xl:gap-10 justify-center items-center text-2xl `} >{navLink.map((link, i) => (<li className='cursor-pointer  duration-200' key={i}>{link.name}</li>))}
 
-                    </li>
-                  ))}</ul>
-              </div>
-            </li>
-            {/* login btn section */}
-            <li>
+          <li className='cursor-pointer  duration-200 group '>
+            <a href="" className='inline-block hover:text-primary font-semibold text-2xl'>
+              <div className='flex items-center gap-[2px] py-2'>Category <span><FaCaretDown className='group-hover:rotate-180 duration-300' /></span></div>
+            </a>
+            <div className='absolute z-50 hidden left-[25%] group-hover:block w-[200px] shadow-md bg-slate-200 p-2'>
+              <ul>{
+
+                dropDownLinks.map(({ id, name, link }) => (
+                  <li key={id}>
+                    <Link to={link} className='inline-block w-full rounded-md p-2  hover:bg-primary/20 font-semibold text-xl'>{name}</Link>
+
+                  </li>
+                ))}</ul>
+            </div>
+          </li>
+          <li>
               <button onClick={HandlePopUP} className='flex items-center bg-secondary text-white px-1 md:px-5 text-nowrap  py-2 hover:scale-105 gap-2 duration-300'><FaUser />My Account</button>
             </li>
-          </ul>
-        </div>
+
+        </ul>
+
       </div>
     </div>
 
